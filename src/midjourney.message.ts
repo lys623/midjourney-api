83c6fde277ffe7d9fc8b604f20101b802ddc0d0a
Yisheng Lu <yisheng.lu@shopee.com>
2023-06-12 11:26:48 +0800

    fix: remove export fetch

--- src/midjourney.message.ts
+++ src/midjourney.message.ts
@@ -9,7 +9,7 @@ import { CreateQueue } from "./queue";
 import { sleep } from "./utls";
 // import fetch from "node-fetch";
 const _importDynamic = new Function('modulePath', 'return import(modulePath)');
-export const fetch = async function (...args: any) {
+const fetch = async function (...args: any) {
     const {default: fetch} = await _importDynamic('node-fetch');
     return fetch(...args);
 }
