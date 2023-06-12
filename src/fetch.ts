63518e216b03612e8ad8abca3a1117c4ab6919ac
Yisheng Lu <yisheng.lu@shopee.com>
2023-06-12 11:36:32 +0800

    fix: fetch

--- src/fetch.ts
+++ src/fetch.ts
@@ -1,5 +1,15 @@
 const _importDynamic = new Function('modulePath', 'return import(modulePath)');
-export const fetch = (async function (...args: any) {
+
+let defaultFetch:any;
+export const fetch = async function (...args: any) {
+    if(!defaultFetch){
+        await main();
+    }
+    return defaultFetch(...args);
+}
+
+async function main(){
     const {default: fetch} = await _importDynamic('node-fetch');
-    return fetch(...args);
-})()
+    defaultFetch=fetch;
+}
+main()
